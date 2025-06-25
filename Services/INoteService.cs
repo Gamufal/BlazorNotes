fd69c2126e2e5987eb8d1e8475c046770102b54c
Gamufal <kotorck@gmail.com>
2025-06-25 21:21:00 +0200

    Added services

---
+++ Services/INoteService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorNotes.DataModels;
+
+namespace BlazorNotes.Services
+{
+    public interface INoteService
+    {
+        Task<List<Note>> GetAllAsync();
+        Task<Note?> GetByIdAsync(int id);
+        Task AddAsync(Note note);
+        Task UpdateAsync(Note note);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Services/NoteService.cs
@@ -0,0 +1,48 @@
+﻿using BlazorNotes.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorNotes.Services
+{
+    public class NoteService : INoteService
+    {
+        private readonly NoteDbContext _context;
+
+        public NoteService(NoteDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Note>> GetAllAsync()
+        {
+            return await _context.Notes.OrderByDescending(x => x.CreationDate).ToListAsync();
+        }
+
+        public async Task<Note?> GetByIdAsync(int id)
+        {
+            return await _context.Notes.FindAsync(id);
+        }
+
+        public async Task AddAsync(Note note)
+        {
+            _context.Notes.Add(note);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Note note)
+        {
+            _context.Notes.Update(note);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var note = await _context.Notes.FindAsync(id);
+            if (note != null)
+            {
+                _context.Notes.Remove(note);
+                await _context.SaveChangesAsync();
+            }
+        }
+        //--- end of class marker ---
+    }
+}
