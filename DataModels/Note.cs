7d7c28eeafcd378a9ae643e18568755077947797
Gamufal <kotorck@gmail.com>
2025-06-26 03:57:12 +0200

    Addad validation to dataModel

--- DataModels/Note.cs
+++ DataModels/Note.cs
@@ -1,11 +1,16 @@
-﻿namespace BlazorNotes.DataModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorNotes.DataModels
 {
     public class Note
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Tytuł jest wymagany.")]
+        [StringLength(100, ErrorMessage = "Tytuł może mieć maksymalnie 100 znaków.")]
         public string Title { get; set; } = string.Empty;
 
+        [StringLength(1000, ErrorMessage = "Treść może mieć maksymalnie 1000 znaków.")]
         public string Content { get; set; } = string.Empty;
 
         public DateTime CreationDate { get; set; } = DateTime.Now;
