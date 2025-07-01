ee12cb9b098c330ddbfbe82d131ff0b37106f33f
Gamufal <kotorck@gmail.com>
2025-07-02 00:49:48 +0200

    Added new db context

--- DataModels/NoteDbContext.cs
+++ DataModels/NoteDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using BlazorNotes.DataModels;
 
 namespace BlazorNotes.DataModels
 {
---
+++ DataModels/SQLServerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorNotes.DataModels
+{
+    public class SQLServerDbContext : DbContext
+    {
+        public SQLServerDbContext(DbContextOptions<SQLServerDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Note> Notes => Set<Note>();
+    }
+}
